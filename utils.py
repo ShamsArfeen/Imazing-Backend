5e495bf766235cbccf6617f1b93eaab7f3061635
Shams <49056370+ShamsArfeen@users.noreply.github.com>
2021-03-31 11:45:20 +0500

    init

---
+++ utils.py
@@ -0,0 +1,30 @@
+def _welcome():
+    return "Imazing API"
+
+def _compact_image():
+    '''Compresses image based on the compression ratio'''
+    return "Compact Image"
+
+def _adjust_saturation():
+    '''Adjusts saturation level of the image'''
+    return "Adjust Saturation"
+
+def _mirror():
+    '''Mirrors image about the x or y axis'''
+    return "Mirror Image"
+
+def _smoothen():
+    '''Smooths the image (blurs the sharp features)'''
+    return "Smooth Image"
+
+def _rotate():
+    '''Rotates the image according to the degrees specified'''
+    return "Rotate Image"
+
+def _alter_rgb():
+    '''Changes the rgb intensities of the image'''
+    return "RGB-Altered Image"
+
+def _sharpen():
+    '''Sharpens the image to bring out more detail'''
+    return "Sharp Image"
