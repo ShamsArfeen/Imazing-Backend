61957b3b9d59c725a0087bd3476d49b31090e557
ubuntu <sa05169@st.habib.edu.pk>
2021-05-10 15:44:51 -0700

    All functions

--- app.py
+++ app.py
@@ -35,7 +35,7 @@ def blur():
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
 
 
-            factor = 0
+            factor = request.args.get('factor', type=float, default=2.0)
 
             im = Image.open(os.path.join(app.config['UPLOAD_FOLDER'], filename))
             enhancer = ImageEnhance.Sharpness(im)
@@ -70,7 +70,7 @@ def sharp():
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
 
 
-            factor = 4
+            factor = request.args.get('factor', type=float, default=4.0)
 
             im = Image.open(os.path.join(app.config['UPLOAD_FOLDER'], filename))
             enhancer = ImageEnhance.Sharpness(im)
@@ -104,7 +104,7 @@ def brighten():
             filename = secure_filename(file.filename)
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
 
-            factor = 1.3
+            factor = request.args.get('factor', type=float, default=1.3)
 
             im = Image.open(os.path.join(app.config['UPLOAD_FOLDER'], filename))
             enhancer = ImageEnhance.Brightness(im)
@@ -138,7 +138,7 @@ def contrast():
             filename = secure_filename(file.filename)
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
 
-            factor = 2
+            factor = request.args.get('factor', type=float, default=2.0)
 
             im = Image.open(os.path.join(app.config['UPLOAD_FOLDER'], filename))
             enhancer = ImageEnhance.Color(im)
@@ -155,3 +155,165 @@ def contrast():
       <input type=submit value=Upload>
     </form>
     '''
+
+    
+@app.route('/halfcompress', methods=['GET', 'POST'])
+def halfcompress():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            
+            factor = request.args.get('factor', type=float, default=2.0)
+
+            im = Image.open(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            x,y = im.size()
+            im.resize((x/factor,y/factor),Image.ANTIALIAS)
+            im.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+
+            return send_from_directory(app.config["UPLOAD_FOLDER"], filename=filename, as_attachment=True)
+    return '''
+    <!doctype html>
+    <title>Choose Image to halfcompress</title>
+    <h1>Choose Image to halfcompress</h1>
+    <form method=post enctype=multipart/form-data>
+      <input type=file name=file>
+      <input type=submit value=Upload>
+    </form>
+    '''
+    
+    
+  
+@app.route('/rightrotate', methods=['GET', 'POST'])
+def rightrotate():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+
+
+            im = Image.open(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            im  = im.transpose(Image.ROTATE_90)
+            im.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+
+            return send_from_directory(app.config["UPLOAD_FOLDER"], filename=filename, as_attachment=True)
+    return '''
+    <!doctype html>
+    <title>Choose Image to rightrotate</title>
+    <h1>Choose Image to rightrotate</h1>
+    <form method=post enctype=multipart/form-data>
+      <input type=file name=file>
+      <input type=submit value=Upload>
+    </form>
+    '''
+    
+@app.route('/leftrotate', methods=['GET', 'POST'])
+def leftrotate():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+
+
+            im = Image.open(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            im  = im.transpose(Image.ROTATE_270)
+            im.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+
+            return send_from_directory(app.config["UPLOAD_FOLDER"], filename=filename, as_attachment=True)
+    return '''
+    <!doctype html>
+    <title>Choose Image to leftrotate</title>
+    <h1>Choose Image to leftrotate</h1>
+    <form method=post enctype=multipart/form-data>
+      <input type=file name=file>
+      <input type=submit value=Upload>
+    </form>
+    '''
+    
+
+@app.route('/fliphorizontal', methods=['GET', 'POST'])
+def fliphorizontal():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+
+
+            im = Image.open(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            im  = im.transpose(Image.FLIP_LEFT_RIGHT)
+            im.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+
+            return send_from_directory(app.config["UPLOAD_FOLDER"], filename=filename, as_attachment=True)
+    return '''
+    <!doctype html>
+    <title>Choose Image to fliphorizontal</title>
+    <h1>Choose Image to fliphorizontal</h1>
+    <form method=post enctype=multipart/form-data>
+      <input type=file name=file>
+      <input type=submit value=Upload>
+    </form>
+    '''
+    
+@app.route('/flipvertical', methods=['GET', 'POST'])
+def flipvertical():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+
+
+            im = Image.open(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            im  = im.transpose(Image.FLIP_TOP_BOTTOM)
+            im.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+
+            return send_from_directory(app.config["UPLOAD_FOLDER"], filename=filename, as_attachment=True)
+    return '''
+    <!doctype html>
+    <title>Choose Image to flipvertical</title>
+    <h1>Choose Image to flipvertical</h1>
+    <form method=post enctype=multipart/form-data>
+      <input type=file name=file>
+      <input type=submit value=Upload>
+    </form>
+    '''
+
