12ffd235a55b222f665b6f10a354486cae5c36a4
Moid Huda <mh05205@st.habib.edu.pk>
2021-05-18 13:34:53 +0500

    updated app.py

--- app.py
+++ app.py
@@ -319,3 +319,65 @@ def flipvertical():
     </form>
     '''
 
+@app.route('/savepng', methods=['GET', 'POST'])
+def save_aspng():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+
+
+            im = Image.open(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            filename = filename.rsplit(".", 1)[0] + ".png"
+            im.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+
+            return send_from_directory(app.config["UPLOAD_FOLDER"], filename=filename, as_attachment=True)
+    return '''
+    <!doctype html>
+    <title>Choose Image to save as png</title>
+    <h1>Choose Image to save as png</h1>
+    <form method=post enctype=multipart/form-data>
+      <input type=file name=file>
+      <input type=submit value=Upload>
+    </form>
+    '''
+
+@app.route('/savejpg', methods=['GET', 'POST'])
+def save_asjpg():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+
+
+            im = Image.open(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            im = im.convert("RGB")
+            filename = filename.rsplit(".", 1)[0] + ".jpg"
+            im.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+
+            return send_from_directory(app.config["UPLOAD_FOLDER"], filename=filename, as_attachment=True)
+    return '''
+    <!doctype html>
+    <title>Choose Image to save as jpg</title>
+    <h1>Choose Image to save as jpg</h1>
+    <form method=post enctype=multipart/form-data>
+      <input type=file name=file>
+      <input type=submit value=Upload>
+    </form>
+    '''
